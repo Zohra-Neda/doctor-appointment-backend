45792f923fa53b069d593c47144944d7939a2eb4
zohra-neda <zohra.neda@gmail.com>
2026-09-13 19:02:32 -0400

    Clean API response

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -2,12 +2,31 @@ class Api::DoctorsController < ApplicationController
   def index
     doctors = Doctor.includes(:category)
 
-    render json: doctors.map { |doctor|
-      doctor.as_json(
-        include: :category
-      ).merge(
-        image_url: doctor.image.attached? ? url_for(doctor.image) : nil
-      )
+    render json: doctors.map { |doctor| doctor_response(doctor) }
+  end
+
+  private
+
+  def doctor_response(doctor)
+    {
+      id: doctor.id,
+      name: doctor.name,
+      address: doctor.address,
+      phone: doctor.phone,
+      years_of_experience: doctor.years_of_experience,
+      patients_count: doctor.patients_count,
+      start_time: doctor.start_time,
+      end_time: doctor.end_time,
+      premium: doctor.premium,
+      about: doctor.about,
+
+      category: {
+        id: doctor.category.id,
+        name: doctor.category.name,
+        icon: doctor.category.icon
+      },
+
+      image_url: doctor.image.attached? ? url_for(doctor.image) : nil
     }
   end
 end
