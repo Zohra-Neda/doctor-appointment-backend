2700948bcab1d03d55f44fd6f9abe89d5e3dccef
zohra-neda <zohra.neda@gmail.com>
2026-09-13 19:12:58 -0400

    Add API route for doctors details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :doctors, only: [:index]
+    resources :doctors, only: [:index, :show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
