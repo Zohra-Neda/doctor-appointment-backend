2d0c800b500f5531ce0d2e9a538215f2d73c893b
zohra-neda <zohra.neda@gmail.com>
2026-09-13 18:33:11 -0400

    Include Active Storage image URLs in Doctors API

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -2,8 +2,12 @@ class Api::DoctorsController < ApplicationController
   def index
     doctors = Doctor.includes(:category)
 
-    render json: doctors.as_json(
-      include: :category
-    )
+    render json: doctors.map { |doctor|
+      doctor.as_json(
+        include: :category
+      ).merge(
+        image_url: doctor.image.attached? ? url_for(doctor.image) : nil
+      )
+    }
   end
 end
