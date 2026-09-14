25b38a924553b9e470b5537cdd69ebf8f2a3e2a7
zohra-neda <zohra.neda@gmail.com>
2026-09-13 21:33:35 -0400

    Clean API response

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -2,6 +2,16 @@ class Api::CategoriesController < ApplicationController
   def index
     categories = Category.all
 
-    render json: categories
+    render json: categories.map { |category| category_response(category) }
+  end
+
+  private
+
+  def category_response(category)
+    {
+      id: category.id,
+      name: category.name,
+      icon: category.icon
+    }
   end
 end
