d62de393ad773e1eeb6fd3fc99d8a000b0dc6221
zohra-neda <zohra.neda@gmail.com>
2026-09-14 10:30:04 -0400

    Add API route for Appoitments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :doctors, only: [:index, :show]
     resources :categories, only: [:index]
+    resources :appointments, only: [:index, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
